a1d6afd7739e75ab6500c6bb5e84897b5eb5dd56
EswarLalith <eshwarlalith04@gmail.com>
2026-08-19 19:16:13 +0530

    about page done

--- frontend/src/app/about/about.ts
+++ frontend/src/app/about/about.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [Header],
+  imports: [Header, RouterLink,RouterLinkActive],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
